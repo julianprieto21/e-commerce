871e98a260a48bcdf42695fd0dec818a18bcee7f
Julian Prieto <prietojulian2003@gmail.com>
2024-05-30 15:31:46 -0300

    add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
+
+// https://astro.build/config
 export default defineConfig({
-   site: 'https://lexingtonthemes.com',
-  integrations: [tailwind(),  sitemap()]
+  site: 'https://lexingtonthemes.com',
+  integrations: [tailwind(), sitemap(), react()]
 });
