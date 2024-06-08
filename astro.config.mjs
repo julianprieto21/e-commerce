68bb0b2c6c336ca58c9bfb03e66b0560cd404a71
Julian Prieto <prietojulian2003@gmail.com>
2024-06-07 21:33:02 -0300

    add vercel ssr adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://lexingtonthemes.com',
   integrations: [tailwind(), sitemap(), react()],
   output: 'server',
+  adapter: vercel()
 });
