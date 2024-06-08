a571d659bf91aca3be9b27fa3b1f7a4b0a7fdcb9
Julian Prieto <prietojulian2003@gmail.com>
2024-06-08 11:00:26 -0300

    remove icon library

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,5 +1,4 @@
 import { useCart } from "@/store/useCart";
-import { Cart as CartIcon } from "iconoir-react";
 import { useEffect, useState } from "react";
 
 export default function Cart() {
@@ -15,7 +14,7 @@ export default function Cart() {
         href="/cart"
         className="shadow cursor-pointer fixed m-8 right-0 top-0 z-10 flex items-center justify-center p-2 text-black bg-color-900 rounded-lg hover:scale-110 transition"
       >
-        <CartIcon />
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" data-icon="SvgShoppingCart" aria-hidden="true"><path d="M20.112 19.4a1.629 1.629 0 11-1.629-1.629 1.63 1.63 0 011.629 1.629zM9.941 17.768a1.629 1.629 0 101.628 1.632 1.629 1.629 0 00-1.628-1.632zM3 3.006h1.678a2.113 2.113 0 011.965 1.573l2.051 9.152a2.114 2.114 0 001.965 1.574h6.788a2.153 2.153 0 001.989-1.568L20.957 8.8a1.233 1.233 0 00-1.236-1.588L11.4 7.064"></path></svg>
         <span className="absolute -right-1 -bottom-1 size-4 px-1 text-[10px] bg-red-500 text-white rounded-full text-center">
           {quantity}
         </span>
--- src/components/MailTo.tsx
+++
@@ -1,9 +0,0 @@
-import { SendMail } from "iconoir-react";
-
-export default function MailTo() {
-  return (
-    <a href="mailto:camisetasnanni@gmail.com?subject=Camisetas Nanni | Consultas" className="cursor-pointer fixed bottom-10 size-12 rounded-md right-10 z-10 grid place-content-center text-color-500 group">
-      <SendMail className="size-10 group-hover:translate-x-3 transition-all" />
-    </a>
-  )
-}
