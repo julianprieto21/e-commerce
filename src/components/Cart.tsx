642aff5864eec0dd9049a1338ba33d805413d07a
Julian Prieto <prietojulian2003@gmail.com>
2024-06-07 21:34:09 -0300

    fix quantity cart items

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -3,7 +3,7 @@ import { Cart as CartIcon } from "iconoir-react";
 import { useEffect, useState } from "react";
 
 export default function Cart() {
-  const [ quantity, setQuantity ] = useState(0);
+  const [quantity, setQuantity] = useState(0);
   const { items } = useCart((state) => state);
 
   useEffect(() => {
@@ -11,14 +11,15 @@ export default function Cart() {
   }, [items]);
   return (
     <>
-    <a
-    href="/cart"
-    className="shadow cursor-pointer fixed m-8 right-0 top-0 z-10 flex items-center justify-center p-2 text-black bg-color-900 rounded-lg hover:scale-110 transition"
-    >     
-     <CartIcon />
-    <span className="absolute -right-1 -bottom-1 size-4 text-xs bg-red-500 text-white rounded-full text-center">{quantity}</span>
-    </a>
+      <a
+        href="/cart"
+        className="shadow cursor-pointer fixed m-8 right-0 top-0 z-10 flex items-center justify-center p-2 text-black bg-color-900 rounded-lg hover:scale-110 transition"
+      >
+        <CartIcon />
+        <span className="absolute -right-1 -bottom-1 size-4 px-1 text-[10px] bg-red-500 text-white rounded-full text-center">
+          {quantity}
+        </span>
+      </a>
     </>
-   
-  )
+  );
 }
